57669269cb8a7d48f765f51f0006b56355d44c1f
roninJosue <roninjosue88@gmail.com>
2021-04-11 19:37:31 -0600

    Caesars Cipher finish

---
+++ javascript_algorithms_data_structure/projects/caesars_cipher.js
@@ -0,0 +1,16 @@
+const rot13 = (str) => {
+  return str.split("").map((letter) => {
+    let charCode = letter.charCodeAt(0);
+    
+    let cipherLetter = letter;
+    if (charCode >= 65 && charCode <= 90) {
+        
+      let cipherCharCode = (charCode % 26) + 65;
+      cipherLetter = String.fromCharCode(cipherCharCode);
+    }
+    
+    return cipherLetter
+  }).reduce((letter, current) => letter += current, '')
+};
+
+console.log(rot13("SERR YBIR?"));
