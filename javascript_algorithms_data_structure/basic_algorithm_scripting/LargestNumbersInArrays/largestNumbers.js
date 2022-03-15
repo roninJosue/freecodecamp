7b3eaddf0337ccb4a8014bf79e0bb7ab001edf0c
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-03-14 21:34:44 -0600

    Largest Numbers in Arras problem (Refactor)

--- javascript_algorithms_data_structure/basic_algorithm_scripting/LargestNumbersInArrays/largestNumbers.js
+++ javascript_algorithms_data_structure/basic_algorithm_scripting/LargestNumbersInArrays/largestNumbers.js
@@ -1,9 +1,5 @@
 const largestNumbers = (arr) => {
-  let largest = []
-  for (let index = 0, len = arr.length; index < len; index++){
-    largest.push(Math.max(...arr[index]))
-  }
-  return largest
+  return arr.map(a => Math.max(...a))
 }
 
 console.log(largestNumbers([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]))
