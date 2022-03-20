75cc53163edc04a148af99185776f7507c261abc
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-03-19 21:52:57 -0600

    Wherefore art thou problem refactor

--- JavascriptAlgorithmsDataStructure/IntermediateAlgorithmScripting/WhereforeArtThou/whatIsInAName.js
+++ JavascriptAlgorithmsDataStructure/IntermediateAlgorithmScripting/WhereforeArtThou/whatIsInAName.js
@@ -1,11 +1,11 @@
 const whatIsInAName = (collection, source) => {
-  let sourceKeys = Object.keys(source)
-  return collection.filter(obj => {
-    return sourceKeys.every(src => Object.keys(obj).indexOf(src) > -1)
-  }).filter(obj => {
-    let test = sourceKeys.filter(key=>obj[key] === source[key])
-    return test.length === sourceKeys.length
-  })
+  const sourceKeys = Object.keys(source)
+  return collection
+    .filter(obj => sourceKeys.every(src => Object.keys(obj).indexOf(src) > -1))
+    .filter(obj => sourceKeys.filter(key => obj[key] === source[key]).length === sourceKeys.length)
 }
 
-console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }))
+console.log(whatIsInAName([{first: "Romeo", last: "Montague"}, {first: "Mercutio", last: null}, {
+  first: "Tybalt",
+  last: "Capulet"
+}], {last: "Capulet"}))
