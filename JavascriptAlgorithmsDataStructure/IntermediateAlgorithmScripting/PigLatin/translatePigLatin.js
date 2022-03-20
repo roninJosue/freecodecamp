43f37b31755225334c6d860170b8a7fbaa63cb56
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-03-20 17:12:30 -0600

    Pig Latin problem refactor

--- JavascriptAlgorithmsDataStructure/IntermediateAlgorithmScripting/PigLatin/translatePigLatin.js
+++ JavascriptAlgorithmsDataStructure/IntermediateAlgorithmScripting/PigLatin/translatePigLatin.js
@@ -1,11 +1,10 @@
 const translatePigLatin = str => {
   const firstLetter = /^[aeiou]/.test(str)
-
-  if (firstLetter) return str += 'way'
-
   const consonants = str.match(/(^[^aeiou]+)/g)
 
-  return str.replace(consonants, '').concat(...consonants, 'ay')
+  return firstLetter ?
+    `${str}way` :
+    `${str.replace(consonants, '')}${consonants}ay`
 }
 
 console.log(translatePigLatin('consonant'))
