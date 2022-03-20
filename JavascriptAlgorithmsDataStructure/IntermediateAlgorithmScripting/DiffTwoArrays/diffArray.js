46e2eb20eb7cbe21742e49ed3991cfcc7bd9e5b6
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-03-19 21:30:16 -0600

    Diff Two Arrays problem refactor

--- JavascriptAlgorithmsDataStructure/IntermediateAlgorithmScripting/DiffTwoArrays/diffArray.js
+++ JavascriptAlgorithmsDataStructure/IntermediateAlgorithmScripting/DiffTwoArrays/diffArray.js
@@ -1,13 +1,6 @@
 const diffArray = (arr1, arr2) => {
-  function arrFilter(arrA, arrB) {
-    return arrA.filter(item => arrB.indexOf(item) < 0)
-  }
-
-  let diff1 = arrFilter(arr1, arr2)
-  let diff2 = arrFilter(arr2, arr1)
-
-  return [...diff1, ...diff2]
-
+  return [...arr1, ...arr2]
+    .filter(item => !arr1.includes(item) || !arr2.includes(item))
 }
 
 console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]))
