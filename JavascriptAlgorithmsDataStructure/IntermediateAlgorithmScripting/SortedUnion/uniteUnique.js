d0ef6c9b372c852e578c44ba1d36d7524f3f088e
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-03-22 20:50:14 -0600

    Sorted Union problem refactor

--- JavascriptAlgorithmsDataStructure/IntermediateAlgorithmScripting/SortedUnion/uniteUnique.js
+++ JavascriptAlgorithmsDataStructure/IntermediateAlgorithmScripting/SortedUnion/uniteUnique.js
@@ -1,13 +1,10 @@
 const uniteUnique = (...arr) => {
-  const temp = []
-  arr.forEach(a => {
-    a.forEach(x => {
-      if (!temp.includes(x)) {
-        temp.push(x)
-      }
-    })
-  })
-  return temp
+  return arr
+    .reduce((acc, curr) => [...acc, ...curr], [])
+    .reduce((acc, curr) => {
+      if (acc.includes(curr)) return acc
+      return [...acc, curr]
+    }, [])
 }
 
 console.log(uniteUnique([1, 3, 2, 3], [5, 2, 1, 4], [2, 1]))
