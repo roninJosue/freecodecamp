eca41b8976da51c663eccfaeb470cebdaf05fad7
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-03-22 21:11:15 -0600

    Convert HTML Entities problem refactor

--- JavascriptAlgorithmsDataStructure/IntermediateAlgorithmScripting/ConvertHTMLEntities/convertHTML.js
+++ JavascriptAlgorithmsDataStructure/IntermediateAlgorithmScripting/ConvertHTMLEntities/convertHTML.js
@@ -1,16 +1,16 @@
 const convertHTML = (str) => {
   const entities = {
-    38: "&amp;",
-    60: "&lt;",
-    62: "&gt;",
-    34: "&quot;",
-    39: "&apos;"
+    '&': "&amp;",
+    '<': "&lt;",
+    '>': "&gt;",
+    '"': "&quot;",
+    "'": "&apos;"
   }
 
-  return str.split("").reduce((html, letter) => {
-    let charCode = letter.charCodeAt(0)
-    return entities.hasOwnProperty(charCode) ? html + entities[charCode] : html + letter
-  }, "")
+  return str.split("")
+    .reduce((html, letter) => entities[letter]
+      ? html + entities[letter]
+      : html + letter, "")
 }
 
 console.log(convertHTML("Hamburgers < Pizza < Tacos"))
