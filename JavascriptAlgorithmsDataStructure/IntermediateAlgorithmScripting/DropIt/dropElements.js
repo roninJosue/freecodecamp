46bdccda9b92ff89f66a328e158663ac4a2415cc
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-03-24 08:41:52 -0600

    Indentation

--- JavascriptAlgorithmsDataStructure/IntermediateAlgorithmScripting/DropIt/dropElements.js
+++ JavascriptAlgorithmsDataStructure/IntermediateAlgorithmScripting/DropIt/dropElements.js
@@ -1,9 +1,11 @@
 const dropElements = (arr, func) => {
   let cond = false
-  return arr.filter(num=>{
+  return arr.filter(num => {
     cond = func(num) ? true : cond
     return cond
   })
 }
 
-console.log(dropElements([0, 1, 0, 1], function(n) {return n === 1;}))
+console.log(dropElements([0, 1, 0, 1], function (n) {
+  return n === 1;
+}))
