edd8e63c54276f49d90f31585edbf1f0eaa0011a
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-03-24 08:40:54 -0600

    Drop it problem removing if

--- JavascriptAlgorithmsDataStructure/IntermediateAlgorithmScripting/DropIt/dropElements.js
+++ JavascriptAlgorithmsDataStructure/IntermediateAlgorithmScripting/DropIt/dropElements.js
@@ -1,9 +1,9 @@
 const dropElements = (arr, func) => {
   let cond = false
   return arr.filter(num=>{
-    if (func(num)) cond = true
+    cond = func(num) ? true : cond
     return cond
   })
 }
 
-console.log(dropElements([1, 2, 3, 4], function(n) {return n >= 3;}))
+console.log(dropElements([0, 1, 0, 1], function(n) {return n === 1;}))
