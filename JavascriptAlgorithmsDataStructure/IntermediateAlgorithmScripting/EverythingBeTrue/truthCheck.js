7ed5153c05e248a1160f5d02a81ea4dcd0850e51
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-03-24 10:33:57 -0600

    Everything Be True problem

---
+++ JavascriptAlgorithmsDataStructure/IntermediateAlgorithmScripting/EverythingBeTrue/truthCheck.js
@@ -0,0 +1,7 @@
+const truthCheck = (collection, pre) => collection.every(obj => !!obj[pre])
+
+console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {
+  name: "Naomi",
+  role: "",
+  isBot: false
+}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot"))
