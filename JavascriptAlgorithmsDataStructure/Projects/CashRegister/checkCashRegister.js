555125438df3dd1b308c6acf238789ad7655267f
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-03-27 17:57:27 -0600

    Format file

--- JavascriptAlgorithmsDataStructure/Projects/CashRegister/checkCashRegister.js
+++ JavascriptAlgorithmsDataStructure/Projects/CashRegister/checkCashRegister.js
@@ -66,5 +66,16 @@ function checkCashRegister(price, cash, cid) {
 }
 
 console.log(
-  checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
+  checkCashRegister(19.5, 20, [
+      ["PENNY", 0.01],
+      ["NICKEL", 0],
+      ["DIME", 0],
+      ["QUARTER", 0],
+      ["ONE", 1],
+      ["FIVE", 0],
+      ["TEN", 0],
+      ["TWENTY", 0],
+      ["ONE HUNDRED", 0]
+    ]
+  )
 );
